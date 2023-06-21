cf308432782ac84bf5e264731f6809cb101fe11a
Nofal475 <nofalelahi413@gmail.com>
2023-06-21 15:43:26 +0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='nrf24-op',
+    version='0.1.0',
+    description='Python package for interacting with NRF24 wireless module',
+    packages=['nrf24_op'],
+    install_requires=['spidev', 'OPi.GPIO']
+)
